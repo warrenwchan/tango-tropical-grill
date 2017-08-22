d41c5c2132aff5888afbba4898d86effb62b9e22
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-08-22 01:06:37 -0700

    add react stage for carousel to display favorites

--- src/components/faveImage/faveImage.js
+++ src/components/faveImage/faveImage.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import styles from './styles.scss';
 
+import faveS2 from '../../assets/favorites/s2-min.jpg'
+import fave13 from '../../assets/favorites/13-min.jpg'
+import fave16 from '../../assets/favorites/16-min.jpg'
+import fave21 from '../../assets/favorites/21-min.jpg'
+import fave24 from '../../assets/favorites/24-min.jpg'
+import fave31 from '../../assets/favorites/31-min.jpg'
+import fave40 from '../../assets/favorites/40-min.jpg'
+import fave42 from '../../assets/favorites/42-min.jpg'
+import fave57 from '../../assets/favorites/57-min.jpg'
+import fave75 from '../../assets/favorites/75-min.jpg'
+
 class faveImage extends Component {
   render() {
 
     const faveBg = {
       backgroundImage: 'linear-gradient(135deg, rgba(26,26,26,0.5) 0%,rgba(26,26,26,0.5) 100%), url('+ this.props.image + ')',
       backgroundPosition: 'center',
-      backgroundSize: '350%'
     }
 
     return (
-      <div className={this.props.imgSize} style={faveBg}>
-        <div className="imgTextContain">
-          <p className="dishName">{this.props.name}</p>
-          <p className="dishNumber" >{this.props.number}</p>
-        </div>
+      <div className="faveImage" style={faveBg}>
+        <p className="dishNumber" >{this.props.number}</p>
+        <p className="dishName">{this.props.name}</p>
       </div>
     );
   }
--- src/containers/imageGrid/imageGrid.js
+++ src/containers/imageGrid/imageGrid.js
@@ -4,20 +4,41 @@ import styles from './styles.scss';
 import FaveImage from '../../components/faveImage/'
 import FaveData from '../../data/index'
 
+import Stage from 'react-stage';
+
+
 class ImageGrid extends Component {
   render() {
+
+    const styles = {
+      boxSizing: 'border-box',
+      display: 'block',
+      width: '100%',
+      padding: '100px',
+      textAlign: 'center'
+    };
+
+    const settings = {
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 1,
+      speed: 500
+    };
+
+    console.log(FaveData.favorites)
+
     return (
-      <div className="imgGrid">
+      <Stage {...settings}>
         {FaveData.favorites.map((fave, i) => {
           return <FaveImage
             key={i}
-            image={fave.image}
             name={fave.name}
             number={fave.number}
-            imgSize={fave.size}
+            image={fave.image}
           />
         })}
-      </div>
+      </Stage>
     );
   }
 }
