12195d921f5d89ba937418cea7384ab61ae438c6
Warren Chan <warrenchan@warren-2.local>
2017-08-22 22:25:03 -0700

    Added React-stage dependency

--- src/containers/imageGrid/imageGrid.js
+++ src/containers/imageGrid/imageGrid.js
@@ -4,18 +4,39 @@ import styles from './styles.scss';
 import FaveImage from '../../components/faveImage/'
 import FaveData from '../../data/index'
 
+import Stage from 'react-stage';
+
 class ImageGrid extends Component {
   render() {
+
+    const styles = {
+      boxSizing: 'border-box',
+      display: 'block',
+      width: '100%',
+      padding: '100px',
+      textAlign: 'center'
+    };
+
+    const settings = {
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 1,
+      speed: 500
+    };
+
     return (
-      <div>
-        {FaveData.favorites.map((fave, i) => {
-          return <FaveImage
-            key={i}
-            name={fave.name}
-            number={fave.number}
-            image={fave.image}
-          />
-        })}
+      <div className="imgGrid">
+        <Stage {...settings}>
+          {FaveData.favorites.map((fave, i) => {
+            return <FaveImage
+              key={i}
+              name={fave.name}
+              number={fave.number}
+              image={fave.image}
+            />
+          })}
+        </Stage>
       </div>
     );
   }
