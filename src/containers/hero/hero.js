05a82641dee62976d35cf058c987d4c4ee7668d6
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-09-07 21:36:37 -0700

    Changed Hero and nav bar css desktop Layout

--- src/containers/hero/hero.js
+++ src/containers/hero/hero.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import styles from './styles.scss';
 
+import NavBar from '../navbar/';
+
 const Hero = () => (
   <div className="heroImageContainer">
-    <div className="heroTitleContain" >
-      <p className="heroTitle">Tango</p>
-      <p className="heroSubTitle" >Tropical Grill <br/> Vietnamese & Thai Cuisine </p>
-      <a className="heroButton" data-scroll href="#contact">Contact</a>
+    <NavBar />
+    <div className="heroBackgroundImage">
+      <div className="heroTitleContain" >
+        <p className="heroTitle">Tango</p>
+        <p className="heroSubTitle" >Tropical Grill <br/> Vietnamese & Thai Cuisine </p>
+        <a className="heroButton" data-scroll href="#contact">Contact</a>
+      </div>
     </div>
   </div>
 )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import NavBar from './../containers/navbar/'
-
 import './index.scss'
-
-const Header = () =>
-
-  <NavBar />
 
 const TemplateWrapper = ({ children }) =>
   <div>
@@ -20,7 +14,6 @@ const TemplateWrapper = ({ children }) =>
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     {children()}
   </div>
 
