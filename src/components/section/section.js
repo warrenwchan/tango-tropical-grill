7f4e6d43687e53ab80849f1e3d13189ca6b7b6d3
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-09-08 00:35:51 -0700

    Section title restyled

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -6,8 +6,10 @@ class Section extends Component {
     return (
       <div id={this.props.id} className="sectionContainer" >
         <div className="sectionTitleContain" >
-          <p className="sectionTitle" >{this.props.title}</p>
-          <img className="sectionIcon" src={this.props.icon} alt="{icon}"/>
+          <div className="sectionTitle">
+            <p>{this.props.title}</p>
+            <img className="sectionIcon" src={this.props.icon} alt="{icon}"/>
+          </div>
           <p className="sectionSubText" >{this.props.subText}</p>
         </div>
         {this.props.children}
