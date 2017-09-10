7d17541e6b933501fe1abf7b6e276da4495513ad
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-09-10 03:19:03 -0700

    removed non existant used of props. Changed preset value in submission form

--- src/components/coupon/coupon.js
+++ src/components/coupon/coupon.js
@@ -30,10 +30,26 @@ class Coupon extends Component {
 
     return(
       <div className="couponContainer">
-        <div id={this.props.id} className="elements" style={couponBG}>
+        <div className="elements" style={couponBG}>
           <div className="elementContent">
             {this.state.toggleForm === true ? (
-              this.state.toggleForm && <SpecialForm key={this.props.key} onClick={this.onClick.bind(this)}/>
+              <div id="specialsForm">
+                <form name="coupon" action="thank-you" data-netlify="true">
+                  <p>
+                    <label>Your Name: <input type="text" name="name"/></label>
+                  </p>
+                  <p>
+                    <label>Your Email: <input type="email" name="email"/></label>
+                  </p>
+                  <p>
+                    <label className="couponNumberValue" ><input type="coupon" name="coupon" value={this.props.title}/></label>
+                  </p>
+                  <div className="buttonContain">
+                    <button className="couponBtn a" type="submit">Send</button>
+                    <button className="couponBtn b" type="button" onClick={this.onClick.bind(this)}>Cancel</button>
+                  </div>
+                </form>
+              </div>
             ):(
               <div id="specialLabel" onClick={this.onClick.bind(this)}>
                 <h3 className="couponTitle">{this.props.title}</h3>
