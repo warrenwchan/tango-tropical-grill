45f8a1e1546bcc6b5a0257ea91740534007a67b6
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-09-10 19:43:54 -0700

    Added switch to conditionally render form or label. Moved Form tag outside of specicalForm div

--- src/components/coupon/coupon.js
+++ src/components/coupon/coupon.js
@@ -30,23 +30,31 @@ class Coupon extends Component {
       <div className="couponContainer">
         <div className="elements" style={couponBG}>
           <div className="elementContent">
-          <div id="specialsForm">
-                <form name="coupon" action="thank-you" data-netlify="true">
-                  <p>
-                    <label>Your Name: <input type="text" name="name"/></label>
-                  </p>
-                  <p>
-                    <label>Your Email: <input type="email" name="email"/></label>
-                  </p>
-                  <p>
-                    <label className="couponNumberValue" ><input type="coupon" name="coupon" defaultValue={this.props.title}/></label>
-                  </p>
-                  <div className="buttonContain">
-                    <button className="couponBtn a" type="submit">Send</button>
-                    <button className="couponBtn b" type="button" onClick={this.onClick.bind(this)}>Cancel</button>
-                  </div>
-                </form>
-              </div>
+            <form name="coupon" action="thank-you" data-netlify="true">
+              {this.state.toggleForm === true ? (
+                <div id="specialsForm">
+                    <p>
+                      <label>Your Name: <input type="text" name="name"/></label>
+                    </p>
+                    <p>
+                      <label>Your Email: <input type="email" name="email"/></label>
+                    </p>
+                    <p>
+                      <label className="couponNumberValue" ><input type="coupon" name="coupon" defaultValue={this.props.title}/></label>
+                    </p>
+                    <div className="buttonContain">
+                      <button className="couponBtn a" type="submit">Send</button>
+                      <button className="couponBtn b" type="button" onClick={this.onClick.bind(this)}>Cancel</button>
+                    </div>
+                </div>
+              ):(
+                <div id="specialLabel" onClick={this.onClick.bind(this)}>
+                  <h3 className="couponTitle">{this.props.title}</h3>
+                  <p className="couponDescription">{this.props.desc}</p>
+                  <p className="couponAcception">*{this.props.acpt}*</p>
+                </div>
+              )}
+            </form>
           </div>
         </div>
       </div>
