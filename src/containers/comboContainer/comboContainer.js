51e80fa621d9ffe9d1b5e6b9759b5dc939e3ce42
Warren Chan <warrenchan@warren-2.hitronhub.home>
2017-09-14 16:24:12 -0700

    reset node modules passed images into combo container

--- src/containers/comboContainer/comboContainer.js
+++ src/containers/comboContainer/comboContainer.js
@@ -4,6 +4,10 @@ import styles from './styles.scss';
 import Section from '../../components/section/';
 import Combo from '../../components/combo/';
 
+import Sub from '../../assets/favorites/s2-min.jpg';
+import Bun from '../../assets/favorites/24-min.jpg';
+import Stir from '../../assets/favorites/40-min.jpg';
+
 class ComboContainer extends Component {
   render() {
     return (
@@ -19,18 +23,21 @@ class ComboContainer extends Component {
             main="Sub from S1 - S"
             sides="2 Sring Rolls or Yam Fries"
             price="$10"
+            image={Sub}
           />
           <Combo
             title="Combo B"
             main="Bun 21, 26 or 28"
             sides="2 Gyozas or Yam Fries"
             price="$14"
+            image={Bun}
           />
           <Combo
             title="Combo C"
             main="Stir-Fry 29, 32, 38"
             sides="2 Sring Rolls or Yam Fries"
             price="$15"
+            image={Stir}
           />
         </div>
       </Section>
